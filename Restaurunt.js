9747a0f45bede12f0568eb2580f8fc14958c68da
jitesh2604 <jiteshpal2604@gmail.com>
2022-07-22 13:58:30 +0530

    day2

---
+++ Restaurunt.js
@@ -0,0 +1,45 @@
+document.querySelector("#firstBox>h2").addEventListener("click",restFunc);
+function restFunc(){
+   window.location.href="https://seminyak.potatohead.co/feast/tanaman/?utm_source=google_ads&utm_medium=webtraffic&utm_campaign=tanaman"
+}
+
+document.querySelector("#link1").addEventListener("click",linkFunc);
+function linkFunc(){
+    window.location.href="https://in.blendedsearch.com/ws?q=canadian%20dedicated%20servers&asid=bs_ch235&mt=b&nw=s&de=c&ap=&ac=23132&cid=15420234620&aid=125485387890&locale=en_IN&ch=674&gclid=EAIaIQobChMI67PA7-2G-QIVFzErCh2oXgTnEAAYASAAEgIQ8vD_BwE"
+}
+
+document.querySelector("#web1").addEventListener("click",webFunc);
+function webFunc(){
+   window.location.href="https://seminyak.potatohead.co/feast/tanaman/?utm_source=google_ads&utm_medium=webtraffic&utm_campaign=tanaman"
+}
+
+document.querySelector("#second").addEventListener("click",secondFunc);
+function secondFunc(){
+    window.location.href="https://www.royyamaguchi.com/"
+}
+
+document.querySelector("#link2").addEventListener("click",link2Func);
+function link2Func(){
+    window.location.href="https://in.blendedsearch.com/ws?q=canadian%20dedicated%20servers&asid=bs_ch235&mt=b&nw=s&de=c&ap=&ac=23132&cid=15420234620&aid=125485387890&locale=en_IN&ch=674&gclid=EAIaIQobChMI67PA7-2G-QIVFzErCh2oXgTnEAAYASAAEgIQ8vD_BwE"
+}
+
+document.querySelector("#web2").addEventListener("click",web2Func);
+function web2Func(){
+    window.location.href="https://www.royyamaguchi.com/"
+}
+
+document.querySelector("#third").addEventListener("click",thirdFunc);
+function thirdFunc(){
+    window.location.href="https://www.harsensislandschoolhousegrille.com/"
+}
+
+document.querySelector("#link3").addEventListener("click",link3Func);
+function link3Func(){
+    window.location.href="https://in.blendedsearch.com/ws?q=canadian%20dedicated%20servers&asid=bs_ch235&mt=b&nw=s&de=c&ap=&ac=23132&cid=15420234620&aid=125485387890&locale=en_IN&ch=674&gclid=EAIaIQobChMI67PA7-2G-QIVFzErCh2oXgTnEAAYASAAEgIQ8vD_BwE"
+}
+
+document.querySelector("#web3").addEventListener("click",web3Func);
+function web3Func(){
+    window.location.href="https://www.harsensislandschoolhousegrille.com/"
+}
+
