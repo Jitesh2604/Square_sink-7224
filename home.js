8b7854553ec3d34f0437b5a4a0fae632b7f5e085
jitesh2604 <jiteshpal2604@gmail.com>
2022-07-22 13:54:20 +0530

    day1

---
+++ home.js
@@ -0,0 +1,22 @@
+document.querySelector("#vpn").addEventListener("click",vpnFunc);
+
+    function vpnFunc(){
+       window.location.href="vpn.html";
+    };
+
+    document.querySelector("#Restarunt").addEventListener("click",restaruntFunc);
+
+    function restaruntFunc(){
+       window.location.href="Restaurunt.html";
+    };
+
+    document.querySelector("#Sonic").addEventListener("click",SonicFunc);
+
+    function SonicFunc(){
+       window.location.href="sonic.html";
+    };
+    document.querySelector("button").addEventListener("click",cartFunc);
+
+    function cartFunc(){
+        window.location.href="addToCartPage.html";
+    };
