2dc17ab708c0d760d052d64c35c2883a10be4abf
jitesh2604 <jiteshpal2604@gmail.com>
2022-07-22 14:01:43 +0530

    day3

---
+++ vpnPage.js
@@ -0,0 +1,44 @@
+document.querySelector("#firstBox").addEventListener("click",firstFunc);
+function firstFunc(){
+     window.location.href="https://www.izito.co.in/ws?q=vpn%20nederland&asid=iz_in_gb_3_gc2_05&mt=b&nw=s&de=c&ap=&ac=13921&cid=12028894680&aid=121808099211&locale=en_IN&gclid=EAIaIQobChMIxt_92rWJ-QIVd5JmAh056Q6xEAAYASAAEgKB_vD_BwE"
+};
+
+document.querySelector("#second").addEventListener("click",secondFunc);
+function secondFunc(){
+     window.location.href="https://www.zapmeta.co.in/ws?q=affordable%20vps&asid=zm_in_gcx_02&mt=b&nw=s&de=c&ap=&ac=23552&cid=15577214128&aid=136762899288&locale=en_IN&ch=855&gclid=EAIaIQobChMIxt_92rWJ-QIVd5JmAh056Q6xEAAYAiAAEgIIvPD_BwE"
+};
+
+document.querySelector("#thirdBox").addEventListener("click",thirdFunc);
+function thirdFunc(){
+     window.location.href="https://in.top10quest.com/ws?q=vps%20server%20usa&asid=tq_in_12&mt=b&nw=s&de=c&ap=&ac=24574&cid=16098328794&aid=131767988766&locale=en_IN&ch=346&gclid=EAIaIQobChMIxt_92rWJ-QIVd5JmAh056Q6xEAAYAyAAEgKDo_D_BwE"
+};
+
+document.querySelector("#link1").addEventListener("click",link1Func);
+function link1Func(){
+     window.location.href="https://in.blendedsearch.com/ws?q=canadian%20dedicated%20servers&asid=bs_ch235&mt=b&nw=s&de=c&ap=&ac=23132&cid=15420234620&aid=125485387890&locale=en_IN&ch=674&gclid=EAIaIQobChMI67PA7-2G-QIVFzErCh2oXgTnEAAYASAAEgIQ8vD_BwE"
+};
+
+document.querySelector("#link2").addEventListener("click",link2Func);
+function link2Func(){
+     window.location.href="https://in.blendedsearch.com/ws?q=canadian%20dedicated%20servers&asid=bs_ch235&mt=b&nw=s&de=c&ap=&ac=23132&cid=15420234620&aid=125485387890&locale=en_IN&ch=674&gclid=EAIaIQobChMI67PA7-2G-QIVFzErCh2oXgTnEAAYASAAEgIQ8vD_BwE"
+};
+
+document.querySelector("#link3").addEventListener("click",link3Func);
+function link3Func(){
+     window.location.href="https://in.blendedsearch.com/ws?q=canadian%20dedicated%20servers&asid=bs_ch235&mt=b&nw=s&de=c&ap=&ac=23132&cid=15420234620&aid=125485387890&locale=en_IN&ch=674&gclid=EAIaIQobChMI67PA7-2G-QIVFzErCh2oXgTnEAAYASAAEgIQ8vD_BwE"
+};
+
+document.querySelector("#web1").addEventListener("click",web1Func);
+function web1Func(){
+     window.location.href="https://www.izito.co.in/ws?q=vpn%20nederland&asid=iz_in_gb_3_gc2_05&mt=b&nw=s&de=c&ap=&ac=13921&cid=12028894680&aid=121808099211&locale=en_IN&gclid=EAIaIQobChMIxt_92rWJ-QIVd5JmAh056Q6xEAAYASAAEgKB_vD_BwE"
+};
+
+document.querySelector("#web2").addEventListener("click",web2Func);
+function web2Func(){
+     window.location.href="https://www.zapmeta.co.in/ws?q=affordable%20vps&asid=zm_in_gcx_02&mt=b&nw=s&de=c&ap=&ac=23552&cid=15577214128&aid=136762899288&locale=en_IN&ch=855&gclid=EAIaIQobChMIxt_92rWJ-QIVd5JmAh056Q6xEAAYAiAAEgIIvPD_BwE"
+};
+
+document.querySelector("#web3").addEventListener("click",web3Func);
+function web3Func(){
+     window.location.href="https://in.top10quest.com/ws?q=vps%20server%20usa&asid=tq_in_12&mt=b&nw=s&de=c&ap=&ac=24574&cid=16098328794&aid=131767988766&locale=en_IN&ch=346&gclid=EAIaIQobChMIxt_92rWJ-QIVd5JmAh056Q6xEAAYAyAAEgKDo_D_BwE"
+};
